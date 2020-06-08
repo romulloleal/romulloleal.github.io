8a7dd33e7cb5039c6bcf2a956318d8201eca6e48
Romullo Leal <romulloleal@gmail.com>
2020-06-08 18:52:10 -0300

    curriculo

---
+++ precache-manifest.e20bfb265de4f7fcc1a5d73781cc7b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217fceed93d68a537e159bda96f052b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0474a46f4b9c7c18e41",
+    "url": "/static/css/main.f3f220ce.chunk.css"
+  },
+  {
+    "revision": "68146a5882a2bce51c39",
+    "url": "/static/js/2.d7a7a9ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7a7a9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0474a46f4b9c7c18e41",
+    "url": "/static/js/main.61543bb9.chunk.js"
+  },
+  {
+    "revision": "c8ee9afb3be42f8e5166",
+    "url": "/static/js/runtime-main.98e87740.js"
+  },
+  {
+    "revision": "f3905f5ebd413b31d3810d5d3c445c11",
+    "url": "/static/media/eu.f3905f5e.png"
+  }
+]);
