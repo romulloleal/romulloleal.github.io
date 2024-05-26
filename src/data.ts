8543ec0b0855c0a70e31b19736cbbd32e0e6d954
Romullo Leal <romulloleal@gmail.com>
2024-05-26 17:29:52 -0300

    migrate from CRA to vite

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,12 @@
+import { translate } from './utils/Translate';
+
 export const workExperience = [
+  {
+    company: 'GetCodes',
+    period: `10/2023 - ${translate('current')}`,
+    title: 'jobGetCodesTitle',
+    desc: 'jobGetCodesDesc',
+  },
   {
     company: 'Nido Tecnologia',
     period: '03/2023 - 10/2023',
@@ -7,16 +15,16 @@ export const workExperience = [
   },
   {
     company: 'Olga',
-    period: '09/2022 - 01/2023',
+    period: '09/2022 - 12/2022',
     title: 'jobOlgaTitle',
     desc: 'jobOlgaDesc',
   },
-  // {
-  //   company: "PlayMatch",
-  //   period: "06/2021 - 05/2021",
-  //   title: "jobPlayMatchTitle",
-  //   desc: "jobPlayMatchDesc",
-  // },
+  {
+    company: 'PlayMatch',
+    period: '06/2022 - 09/2022',
+    title: 'jobPlayMatchTitle',
+    desc: 'jobPlayMatchDesc',
+  },
   {
     company: 'Dart Digital',
     period: '06/2021 - 05/2021',
--- src/utils/Translate/index.ts
+++ src/utils/Translate/index.ts
@@ -1,44 +1,43 @@
-import brazilFlag from '../../assets/brazil.svg'
-import usaFlag from '../../assets/USA.svg'
-
-import en_us from './languages/en_us.json'
-import pt_br from './languages/pt_br.json'
+import en_us from './languages/en_us.json';
+import pt_br from './languages/pt_br.json';
+import brazilFlag from '@/assets/brazil.svg';
+import usaFlag from '@/assets/USA.svg';
 
 export const languages = [
   {
     flag: usaFlag,
     name: 'en-US',
     file: en_us,
-    desc: 'languageDescriptionEnUs'
+    desc: 'languageDescriptionEnUs',
   },
   {
     flag: brazilFlag,
     name: 'pt-BR',
     file: pt_br,
-    desc: 'languageDescriptionPtBr'
+    desc: 'languageDescriptionPtBr',
   },
-]
+];
 
 const getCurrentLanguage = () => {
   const locale = localStorage.getItem('@romullolealCurriculo:language')
     ? localStorage.getItem('@romullolealCurriculo:language')
-    : navigator.language
+    : navigator.language;
 
   const result = languages.find(
     (language: { name: string; file: unknown }) => language.name === locale
-  )
+  );
 
-  return result || { name: 'en-US', file: en_us }
-}
+  return result || { name: 'en-US', file: en_us };
+};
 
 export const translate = (value: string) => {
-  const translations = getCurrentLanguage().file
-  if (!translations[value as keyof typeof translations]) return value
-  return translations[value as keyof typeof translations]
-}
+  const translations = getCurrentLanguage().file;
+  if (!translations[value as keyof typeof translations]) return value;
+  return translations[value as keyof typeof translations];
+};
 
-export const currentLanguage = getCurrentLanguage().name
+export const currentLanguage = getCurrentLanguage().name;
 export const setLanguage = (value: string) => {
-  localStorage.setItem('@romullolealCurriculo:language', value)
-  window.location.reload()
-}
+  localStorage.setItem('@romullolealCurriculo:language', value);
+  window.location.reload();
+};
